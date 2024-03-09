f0513e5377c317e56b56e9c855b974a81a2bf1a2
rfukuhara <mekabu0919@gmail.com>
2024-03-10 07:22:16 +0900

    first commit

---
+++ discordbot.py
@@ -0,0 +1,37 @@
+# インストールした discord.py を読み込む
+import os
+import discord
+
+# 自分のBotのアクセストークンに置き換えてください
+TOKEN = os.environ["TOKEN"]
+GUILD_ID = os.environ["GUILDID"]
+# 接続に必要なオブジェクトを生成
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+tree = discord.app_commands.CommandTree(client)
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+    await tree.sync(guild=discord.Object(id=GUILD_ID))
+
+@tree.command(name="ishikoro", description="話しかける")
+@discord.app_commands.guilds(GUILD_ID)
+async def talk(interaction: discord.Interaction, message: str = ""):
+    if message == "":
+        await interaction.response.send_message("何？")
+        return
+    if message in ["いぬ", "犬", "inu", "イヌ"]:
+        await interaction.response.send_message("わおん")
+        return
+    if message in ["ねこ", "neko", "猫", "ネコ"]:
+        await interaction.response.send_message("にゃーん")
+        return
+    await interaction.response.send_message("よくわかんねぇな")
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
