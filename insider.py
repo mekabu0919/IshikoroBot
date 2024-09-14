ed62a90b9020cf1616e9a4ee1228dc4776ba320d
rfukuhara <mekabu0919@gmail.com>
2024-09-15 02:10:58 +0900

    Added player addition

--- insider.py
+++ insider.py
@@ -1,24 +1,40 @@
 import random
 
+from discord import User
+
 
 class Player:
-    def __init__(self, name: str) -> None:
-        self.name = name
+    def __init__(self, user: User) -> None:
+        self.user = user
         self.role = None
         self.target = None
+
+    def assign_role(self, role: str):
+        self.role = role
+        return self
 
 
 class GameMaster:
     def __init__(self) -> None:
-        pass
+        self.players = []
 
-    def start(self, targets: list[str]):
+    def add(self, *users: User):
+        for user in users:
+            player = Player(user)
+            self.players.append(player)
+
+    def select_target(self, targets: list[str]):
         self.target = random.choice(targets)
 
-    def assign_role(self, player: Player, role: str):
-        roled_player = player
-        roled_player.role = role
-        return roled_player
+    def shuffle_roles(self):
+        random.shuffle(self.players)
+        self.players[0].assign_role("master")
+        self.players[1].assign_role("インサイダー")
+        for player in self.players[2:]:
+            player.assign_role("市民")
     
     def make_message(self, player: Player):
-        return f"Your role is '{player.role}'"
+        if player.role == "市民":
+            return f"Your role is '{player.role}'"
+        else:
+            return f"Your role is '{player.role}'. The target is '{self.target}'"
--- test_insider.py
+++ test_insider.py
@@ -1,28 +1,53 @@
+from unittest.mock import MagicMock
 import pytest
-
+from discord import User
 from insider import GameMaster, Player
 
 
-def test_お題のリストからお題を決定する():
-    game_master = GameMaster()
-    game_master.start(["ゲーム"])
+@pytest.fixture
+def game_master():
+    def mock_user(id: int, name: str):
+        user = MagicMock(spec=User)
+        user.id = id
+        user.name = name
+        return user
+    gamemaster = GameMaster()
+    users = [mock_user(id, name) for id, name in zip([0, 1, 2, 3], "ABCD")]
+    gamemaster.add(*users)
+    return gamemaster
+
+
+def test_複数のユーザーを入力として受け取りプレイヤーに登録する(game_master):
+    assert game_master.players[0].user.id == 0
+    assert game_master.players[1].user.id == 1
+
+
+def test_お題のリストからお題を決定する(game_master):
+    game_master.select_target(["ゲーム"])
     assert game_master.target == "ゲーム"
 
 
-def test_プレイヤーAにマスターロールを割り当てる():
-    game_master = GameMaster()
-    player_a = Player("A")
-    role_master = "master"
-    roled_player = game_master.assign_role(player_a, role_master)
-    assert roled_player.name == "A"
+def test_プレイヤーAにマスターロールを割り当てる(game_master):
+    roled_player = game_master.players[0].assign_role("master")
+    assert roled_player.user.id == 0
     assert roled_player.role == "master"
 
 
-def test_市民のプレイヤーにロールを通知するメッセージを作成する():
-    game_master = GameMaster()
-    player_a = Player("A")
-    master_player_a = game_master.assign_role(player_a, "master")
-    message = game_master.make_message(master_player_a)
-    assert message == "Your role is 'master'"
+def test_市民のプレイヤーにロールを通知するメッセージを作成する(game_master):
+    villager_player = game_master.players[0].assign_role("市民")
+    message = game_master.make_message(villager_player)
+    assert message == "Your role is '市民'"
 
 
+def test_市民以外のプレイヤーにロールとお題を通知するメッセージを作成する(game_master):
+    game_master.select_target(["ゲーム"])
+    insider_playaer = game_master.players[0].assign_role("インサイダー")
+    message = game_master.make_message(insider_playaer)
+    assert message == "Your role is 'インサイダー'. The target is 'ゲーム'"
+
+
+def test_プレイヤーリストを並び替えてmasterインサイダー市民を割り当てる(game_master):
+    game_master.shuffle_roles()
+    assert game_master.players[0].role == "master"
+    assert game_master.players[1].role == "インサイダー"
+    assert game_master.players[2].role == "市民"
